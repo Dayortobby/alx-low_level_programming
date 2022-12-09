7720258030baaac3511387275043314c04c56275
Dayortobby <dayortobby@gmail.com>
2022-12-09 04:59:30 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlid.h>
 
 /**
  * main - Entry
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
