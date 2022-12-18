26d1b1ba79ea0ffad443c98d76f4b1cd985c9ddb
Dayortobby <dayortobby@gmail.com>
2022-12-17 22:43:46 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		Return(1);
+	else
+		Return(0);
+}
